62dd9fc146d55095fc04208f987c2a93e0d980f4
ggualiato <ggualiato@gmail.com>
2017-11-12 01:48:36 -0200

    details

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
 
 app.use((req, res, next) => {
   var now = new Date().toString();
